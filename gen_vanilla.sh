78f526077ddb8434e30ea814ff570960bae79198
Zhengbao Jiang <zhengbaj@cs.cmu.edu>
2021-08-08 03:40:00 -0400

    added wikisent data

--- gen_vanilla.sh
+++ gen_vanilla.sh
@@ -1,5 +1,5 @@
-output_dir=/mnt/root/TaBERT/data/train_data/wholetable_3merge_bart_mlm_contextmention_ep20
-input_dir=/mnt/root/TaBERT/data/grappa/totto_tablefact_wikisql_train_preprocessed_mention.jsonl
+output_dir=/mnt/root/TaBERT/data/train_data/wholetable_wikisent_tapas_500k_bart_mlm_contextmention
+input_dir=/mnt/root/TaBERT/data/wikipedia_sent/preprocessed_tapas_retbycontext_500k.jsonl
 # --no_shuffle is needed for dev/test
 additional_row_count=0
 top_row_count=100
@@ -16,7 +16,7 @@ for (( i=0; i<${worldsize}; ++i)); do
     --train_corpus ${input_dir} \
     --base_model_name facebook/bart-base \
     --do_lower_case \
-    --epochs_to_generate 20 \
+    --epochs_to_generate 10 \
     --max_context_len 128 \
     --max_column_len 15 \
     --max_cell_len 15 \

7987f543fe5cface1377a31ad5e9bb5ee3a0f141
jzb <zhengbaj@cs.cmu.edu>
2022-01-13 00:32:03 -0500

    updated

--- sql2nl_verify.sh
+++ sql2nl_verify.sh
@@ -5,12 +5,10 @@ source env_initialize.sh
 num_gpu=$1
 case=$2  # 16 128 1024
 model=$3  # 128: wholetable_tapas_samepagehardmatchmax_tapex_sqlandnl_num128_large_wtqqa_strict_128_ep50 bart_large_wtqqa_strict_128_ep50
-          # 16: tapex_large_wtqqa_strict_16_ep50
-          # 1024: tapex_large_wtqqa_strict_1024_ep50
+          # 16: tapex_large_wtqqa_strict_16_ep50 bart_large_wtqqa_strict_16_ep50
+          # 1024: tapex_large_wtqqa_strict_1024_ep50 bart_large_wtqqa_strict_1024_ep50
 alias=$4
 
-to_skip_file=/mnt/root/TaBERT/data/wikitablequestions/tapex/train.src.${case}
-nl_prep_file=/mnt/root/TaBERT/data/wikitablequestions/tapex/sql2nl_denormalized/train.src.num${case}.beamsearchnl
 nl_tabert_dir=wholetable_wtq_wtqnl_denormalized_num${case}_beamsearch  # to be verified
 out_file=/mnt/root/TaBERT/data/train_data/${nl_tabert_dir}/${model}/loss.txt
 
@@ -23,6 +21,8 @@ out_file=/mnt/root/TaBERT/data/train_data/${nl_tabert_dir}/${model}/loss.txt
   --mode computeloss-train \
   --output_file ${out_file}
 
+to_skip_file=/mnt/root/TaBERT/data/wikitablequestions/tapex/train.src.${case}
+nl_prep_file=/mnt/root/TaBERT/data/wikitablequestions/tapex/sql2nl_denormalized/train.src.num${case}.beamsearchnl
 filter_file=/mnt/root/TaBERT/data/wikitablequestions/tapex/sql2nl_denormalized/train.src.num${case}.beamsearchnl.${alias}_filtered
 
 python -m utils.prep --task select_by_loss \
